427e97b892f1582d9a60c2625c72d6e178918282
Jonathan Irwin <jonoirwinrsa@gmail.com>
2022-05-03 14:19:35 +0100

    fix: get basic sql to run

--- models/shopify__customers.sql
+++ models/shopify__customers.sql
@@ -1,33 +1,10 @@
-with commit_comments_customer as (
+with customers as (
     select
         id as customer_id,
-        url,
-        type,
-        login as customer_name
-    from {{ var('commit_comments_customer') }}
-),
-
-commit_comment_reactions_customer as (
-    select
-        id as customer_id,
-        url,
-        type,
-        login as customer_name
-    from {{ var('commit_comment_reactions_customer') }}
-),
-
-customers_union as (
-    select * from commit_comments_customer
-    union all
-        select * from commit_comment_reactions_customer
-    union all
-        select * from issue_comment_reactions_customer
-),
-
-customers as (
-    select *
-    from customers_union
-    group by 1,2
+        first_name,
+        last_name,
+        email
+    from {{ var('customers') }}
 )
 
 select * from customers
